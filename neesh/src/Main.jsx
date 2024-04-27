925309c8228b03c311f86cb5a9acb854557d44fb
jacobkavanal <jake.kavanal@gmail.com>
2024-04-27 15:39:05 -0700

    added main react component

---
+++ neesh/src/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./main.css";
+
+export const Box = () => {
+  return (
+    <div className="box">
+      <div className="group">
+        <div className="text-wrapper">neesh</div>
+        <p className="div">How niche is your interest?</p>
+        <textarea className="rectangle" />
+      </div>
+    </div>
+  );
+};
