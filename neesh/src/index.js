b64186904ba4530f4ac5eed99d8aff46db38e8fc
jacobkavanal <jake.kavanal@gmail.com>
2024-04-28 13:45:04 -0700

    fixed react not opening localhost

--- neesh/src/index.js
+++ neesh/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import { Box } from './Main';
+import { Box } from './Main.jsx'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Box/>
+  <React.StrictMode>  
+    <Box />
   </React.StrictMode>
 );
 
