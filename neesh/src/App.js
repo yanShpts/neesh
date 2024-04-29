de6b0a231c8e7f15ff6c681be76eb416e28d3705
jacobkavanal <jake.kavanal@gmail.com>
2024-04-28 17:35:47 -0700

    updated frontend template

--- neesh/src/App.js
+++ neesh/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./main.css";
+import "./Lead.css"
 
-function App() {
+
+export const Main = () => {
+  const [text, setText] = useState('');
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Logic to submit text to the API (replace with your implementation)
+    console.log('Text to submit:', text);
+    setText(''); // Clear text input after submission
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div className="group">
+        <div className="text-wrapper">neesh</div>
+        <p className="div">How niche is your interest?</p>
+        <form onSubmit={handleSubmit}> {}
+          <textarea 
+            className="rectangle" 
+            value={text} 
+            onChange={handleInputChange} 
+          />
+          <button type="submit" className="submit-button">Submit</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export const Lists = () => {
+  return(
+    <div className='scores'>
+      <div className='Leaderboard'>
+        <div className='title'>Leaderboard</div>
+        <ul className='list'>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+        </ul>
+      </div>
+      <div className='Leaderboard'>
+        <div className='title'>Last Entries</div>
+        <ul className='list'>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+          <li>Item: Score</li>
+        </ul>
+      </div>
+    </div>
+
+
+  )
+}
